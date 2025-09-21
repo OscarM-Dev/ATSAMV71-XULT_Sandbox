071e4e5d495e776909ffd7bec23f1163b1034fe4
David Michel <david.michelt@iteso.mx>
2025-09-20 23:39:40 -0600

    Added button interrupt logic to main.c

--- SAMV71x/app/01_scheduler/src/main.c
+++ SAMV71x/app/01_scheduler/src/main.c
@@ -24,10 +24,45 @@
 
 /*~~~~~~  Local definitions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
+/** IRQ priority for PIO (The lower the value, the greater the priority) */
+#define IRQ_PRIOR_PIO    0
+
+/*~~~~~~  Local variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+static const Pin pinPB1 = PIN_PUSHBUTTON_0;
+
 /*~~~~~~  Global variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 
 /*~~~~~~  Local functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+static void _Button_Handler( const Pin* pPin )
+{
+	if ( pPin == &pinPB1 ) {
+		printf("Button SW0 event confirmed\n\r");
+	}
+}
+
+static void _ConfigureButtons( void )
+{
+	/* Initialize PIO interrupts with priority */
+	PIO_InitializeInterrupts( IRQ_PRIOR_PIO );
+
+	/* Configure PIO as inputs. */
+	PIO_Configure( &pinPB1, 1 ) ;
+
+	/* Adjust PIO denounce filter parameters, uses 10 Hz filter. */
+	PIO_SetDebounceFilter( &pinPB1, 10 ) ;
+
+	/* Initialize PIO interrupt handlers, see PIO definition in board.h. */
+	PIO_ConfigureIt( &pinPB1, _Button_Handler ) ; /* Interrupt on rising edge  */
+
+	/* Enable PIO controller IRQs. */
+	NVIC_EnableIRQ( (IRQn_Type)pinPB1.id ) ;
+
+	/* Enable PIO line interrupts. */
+	PIO_EnableIt( &pinPB1 ) ;
+}
 
 /*----------------------------------------------------------------------------
  *        Exported functions
@@ -41,17 +76,28 @@ extern int main( void )
 {
 	/* Disable watchdog */
 	vfnWdtCtrl_Disable();
+
+	/* Starting program */
+	printf( "\n\r-- Starting program Project1_Task_Scheduler %s --\n\r", SOFTPACK_VERSION ) ;
+	printf( "-- %s\n\r", BOARD_NAME ) ;
+	printf( "-- Compiled: %s %s With %s--\n\r", __DATE__, __TIME__ , COMPILER_NAME);
+
 	/* Enable I and D cache */
 	SCB_EnableICache();
 	/* SCB_EnableDCache(); */
+	
 	/* Configure LEDs */
 	vfnLedCtrl_Configure(); 
+
 	/* Configure Non-preemtive scheduler */
 	vfnScheduler_Init();
+	
 	/* Start scheduler */
 	vfnScheduler_Start();
-	
- 
+
+	/* Configure button interrupts */
+	_ConfigureButtons();
+
 	/* Once all the basic services have been started, go to infinite loop to serviced activated tasks */
 	for(;;)
     {
