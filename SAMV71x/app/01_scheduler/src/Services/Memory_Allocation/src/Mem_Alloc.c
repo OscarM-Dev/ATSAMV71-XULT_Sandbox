c683f0e1b0e79502cbfe3f1693b0d1586adb41ac
Oscar Mercado <omercadorico@gmail.com>
2025-10-04 17:42:23 -0500

    Adding mem alloc function, init of control structure pending

--- SAMV71x/app/01_scheduler/src/Services/Memory_Allocation/src/Mem_Alloc.c
+++ SAMV71x/app/01_scheduler/src/Services/Memory_Allocation/src/Mem_Alloc.c
@@ -10,9 +10,15 @@
 #include "Mem_Alloc.h"
 
 /* ************************************************************************** */
+/* Private macros  
+/* ************************************************************************** */
+#define ADDR_ALIGN_BYTES    4   //Number of bytes to allign addresses.
+#define ALIGN_4BYTES( addr ) ( ( uint8_t * ) ( ( ( ( uint32_t ) addr + ( ADDR_ALIGN_BYTES - 1 ) ) / ADDR_ALIGN_BYTES ) * ADDR_ALIGN_BYTES ) )
+
+/* ************************************************************************** */
 /* Private data.  
 /* ************************************************************************** */
-
+static MemHandlerType MemControl;
 
 /* ************************************************************************** */
 /* Functions.  
@@ -28,5 +34,31 @@
  */
 MemReturnType Mem_Alloc( MemSizeType Size )
 {
+    uint8_t *InitialAddr = 0;
 
+    //Check if there is available space in heap_memalloc to allocate memory space.
+    if ( ( Size > 0 ) && ( Size <= MemControl.FreeBytes ) )
+    {   //Available space.
+        InitialAddr = MemControl.CurrAddr;  //Initial address of new memory space.
+
+        //Update current address.
+        MemControl.CurrAddr += Size;
+
+        //Align new current address with 4 bytes.
+        if ( ( uint32_t ) MemControl.CurrAddr % 4 != 0 )
+        {   //Not a valid address, round to closer address aligned.
+            MemControl.CurrAddr =  ALIGN_4BYTES( MemControl.CurrAddr );
+        }
+
+        //Checking if aligned address exceeds final address of heap_memalloc.
+        if ( MemControl.CurrAddr > MemControl.MemEnd )
+        {   //Exceeds.
+            MemControl.CurrAddr = MemControl.MemEnd;
+        }
+
+        //Update Free bytes.
+        MemControl.FreeBytes = MemControl.MemEnd - MemControl.CurrAddr;
+    }
+
+    return ( MemReturnType ) InitialAddr;
 }
