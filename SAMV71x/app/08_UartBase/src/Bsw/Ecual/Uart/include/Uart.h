404c67db6c70d8e5c81f3028297e967ea758c9f9
Oscar Mercado <omercadorico@gmail.com>
2025-10-19 13:59:20 -0500

    Interrupt and ISR support added for UART transmission.

--- SAMV71x/app/08_UartBase/src/Bsw/Ecual/Uart/include/Uart.h
+++ SAMV71x/app/08_UartBase/src/Bsw/Ecual/Uart/include/Uart.h
@@ -16,6 +16,7 @@
 	#include "Std_Types.h"
   #include "Uart_Types.h"
 	#include "Uart_Cfg.h"
+  #include "Uart_Driver.h"
 
   /* Uart Initialization Fucntion */
   extern void Uart_Init( const uint8_t * ChannelConfigure );
