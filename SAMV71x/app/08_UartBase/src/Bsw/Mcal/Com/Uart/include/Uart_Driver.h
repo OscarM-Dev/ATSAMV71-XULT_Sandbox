acb6a8450749bdf9c1b2a782c88a3cd148676a5f
Oscar Mercado <omercadorico@gmail.com>
2025-10-19 12:19:09 -0500

    Adding register configuration to MCAL driver

--- SAMV71x/app/08_UartBase/src/Bsw/Mcal/Com/Uart/include/Uart_Driver.h
+++ SAMV71x/app/08_UartBase/src/Bsw/Mcal/Com/Uart/include/Uart_Driver.h
@@ -14,7 +14,7 @@
 /* ************************************************************************** */
 /* Public function prototypes  
 /* ************************************************************************** */
-void UART_Driver_Configure( Uart *uart, uint8_t mode, uint8_t clkSrc, uint8_t parity, uint32_t baudrate, uint32_t clkSrcFreq );
+void UART_Driver_Configure( Uart *uart, uint16_t mode, uint16_t clkSrc, uint16_t parity, uint32_t baudrate, uint32_t clkSrcFreq );
 void UART_Driver_SetBaudrate( Uart *uart, uint32_t baudrate, uint32_t clkSrcFreq );
 void UART_Driver_SetIt( Uart *uart, uint8_t enable, uint32_t mode );
 void UART_Driver_SetTx( Uart *uart, uint8_t enable );
--- SAMV71x/app/08_UartBase/src/Bsw/Mcal/Com/Uart/src/Uart_Driver.c
+++ SAMV71x/app/08_UartBase/src/Bsw/Mcal/Com/Uart/src/Uart_Driver.c
@@ -23,9 +23,19 @@
  * @param baudrate Desired baudrate in hz.
  * @param clkSrcFreq Peripheral clock source frequency in hz.
  */
-void UART_Driver_Configure( Uart *uart, uint8_t mode, uint8_t clkSrc, uint8_t parity, uint32_t baudrate, uint32_t clkSrcFreq )
+void UART_Driver_Configure( Uart *uart, uint16_t mode, uint16_t clkSrc, uint16_t parity, uint32_t baudrate, uint32_t clkSrcFreq )
 {
+	//Reset and disable receiver & transmitter
+	uart->UART_CR = UART_CR_RSTRX | UART_CR_RSTTX | UART_CR_RXDIS | UART_CR_TXDIS | UART_CR_RSTSTA;
 
+    //Disabling all interrupts.
+	uart->UART_IDR = UART_IDR_RXRDY | UART_IDR_TXRDY | UART_IDR_OVRE | UART_IDR_FRAME | UART_IDR_PARE | UART_IDR_TXEMPTY | UART_IDR_CMP;
+
+	//Configure mode register.
+	uart->UART_MR = ( mode << 14 ) | ( clkSrc << 12 ) | ( parity << 9 ) | UART_MR_FILTER_DISABLED;
+
+	//Configure default baudrate.
+    UART_Driver_SetBaudrate( uart, baudrate, clkSrcFreq );
 }
 
 /**
@@ -36,8 +46,9 @@ void UART_Driver_Configure( Uart *uart, uint8_t mode, uint8_t clkSrc, uint8_t pa
  * @param clkSrcFreq Peripheral clock source frequency in hz.
  */
 void UART_Driver_SetBaudrate( Uart *uart, uint32_t baudrate, uint32_t clkSrcFreq )
-{
-
+{   
+    uart->UART_CR = UART_CR_RSTTX;
+    uart->UART_BRGR = clkSrcFreq / ( baudrate * 16 );
 }
 
 /**
@@ -49,7 +60,15 @@ void UART_Driver_SetBaudrate( Uart *uart, uint32_t baudrate, uint32_t clkSrcFreq
  */
 void UART_Driver_SetIt( Uart *uart, uint8_t enable, uint32_t mode )
 {
+    if ( enable )
+    {
+        uart->UART_IER = mode;
+    }
 
+    else
+    {
+        uart->UART_IDR = mode;
+    }
 }
 
 /**
@@ -60,7 +79,15 @@ void UART_Driver_SetIt( Uart *uart, uint8_t enable, uint32_t mode )
  */
 void UART_Driver_SetTx( Uart *uart, uint8_t enable )
 {
+    if ( enable )
+    {
+        uart->UART_CR = UART_CR_TXEN;
+    }
 
+    else
+    {
+        uart->UART_CR = UART_CR_TXDIS;
+    }
 }
 
 /**
@@ -72,7 +99,7 @@ void UART_Driver_SetTx( Uart *uart, uint8_t enable )
  */
 void UART_Driver_SendByte( Uart *uart, uint8_t data )
 {
-
+    uart->UART_THR = data;
 }
 
 /**
@@ -82,6 +109,18 @@ void UART_Driver_SendByte( Uart *uart, uint8_t data )
  * @retval TxReady flag value.
  */
 uint8_t UART_Driver_IsTxReady( Uart *uart )
-{
+{   
+    uint8_t result = 0;
 
+    if ( uart->UART_SR & UART_SR_TXRDY )
+    {
+        result = 1;
+    }
+
+    else
+    {
+        result = 0;
+    }
+
+    return result;
 }
